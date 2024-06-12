353d35fd409b59647c002cad5a0b84792bf61bfc
navya04062003 <154512617+navya04062003@users.noreply.github.com>
2024-06-12 06:25:28 +0530

    Update run.py

--- run.py
+++ run.py
@@ -2,27 +2,17 @@ from Core.data import Data
 from Core.config import Column
 from Core.model import LSTM_Trainer
 
-def main():
-    # Initialize Data object
-    data = Data()
-    data.read('AAPL.csv')
-    data.check_null_values()
-    data.clean_data()
-    print(Column.OPEN.value)
-    data.print_head()
-    data.print_description()
-    dataframe, scaler = data.normalize()
-    data.visualize(Column.OPEN.value)
-    data.visualize(Column.CLOSE.value)
+data = Data()
+data.read('Data/AAPL.csv')
+data.check_null_values()
+data.clean_data()
+data.print_head()
+data.print_description()
+data.normalize()
+data.visualize(Column.OPEN.value)
+data.visualize(Column.CLOSE.value)
 
-    # Initialize LSTM Trainer
-    trainer = LSTM_Trainer(dataframe, scaler)
-    trainer.build_and_train_lstm()
-    trainer.predict_and_plot()
-    trainer.evaluate_model()
-
-    # Save the model
-    trainer.save_model('trained_model.pkl')
-
-if __name__ == "__main__":
-    main()
+trainer = LSTM_Trainer(data.dataframe, data.scaler)
+trainer.build_and_train_lstm()
+trainer.predict_and_plot()
+trainer.evaluate_model()
